6757eacda80610c3094a7127d35dbe84b98fb12f
Cristian Saenz <71958786+csaenzc09@users.noreply.github.com>
2021-10-26 12:34:28 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 let header = document.querySelector(`#header`); 
 let toogle = document.querySelector(`.toogle`);
 let enlace = document.querySelector(`nav ul`);
+ let titulo = document.querySelector(`.inicio h1`);
+ let servicios = document.querySelector(`.contenedor_servicios`);
 
-toogle.onclick = ()=>{
+
+ titulo.textContent = "market";
+ titulo.style.color = "teal"; 
+
+
+toogle.addEventListener(`click` , ()=>{
     header.classList.toggle("activo");
-};
+});
 
 enlace.addEventListener( `click` , () =>{
     header.classList.remove("activo");
-})
+});
+
+servicios.children[2].style.display = "none";
+
